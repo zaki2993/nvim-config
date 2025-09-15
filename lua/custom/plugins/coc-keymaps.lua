9871a2e6cb79c985f0722f1ac443a9d14901b3cf
zakaryae <zakaprog@gmail.com>
2025-09-15 09:37:41 +0100

    adding a shortcut for auto implm all methods of an interface ( cursor in inter imp name in the class <A-CR>

--- lua/custom/plugins/coc-keymaps.lua
+++ lua/custom/plugins/coc-keymaps.lua
@@ -9,6 +9,11 @@ return {
 
       vim.api.nvim_set_keymap('i', '<S-Tab>', 'pumvisible() ? "\\<C-p>" : "\\<C-h>"', { noremap = true, expr = true, silent = true })
 
+      -- Direct key for Code Action (Alt+Enter style)
+      vim.api.nvim_set_keymap('n', '<A-CR>', '<Plug>(coc-codeaction)', { silent = true })
+      vim.api.nvim_set_keymap('x', '<A-CR>', '<Plug>(coc-codeaction-selected)', { silent = true })
+      vim.api.nvim_set_keymap('n', '<leader>qf', '<Plug>(coc-fix-current)', { silent = true })
+
       -- Use <C-o> to confirm current suggestion (first by default)
       vim.api.nvim_set_keymap('i', '<C-o>', 'coc#pum#visible() ? coc#pum#confirm() : "\\<C-o>"', { noremap = true, expr = true, silent = true })
     end,
